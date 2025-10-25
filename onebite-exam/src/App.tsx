3a27ae02aacdf16d5d4819081276e8b585554282
shs-0108 <csohee108@gmail.com>
2025-10-25 17:09:06 +0900

    React Router 설정 및 복습

--- onebite-exam/src/App.tsx
+++ onebite-exam/src/App.tsx
@@ -1,130 +1,27 @@
+import { Outlet, Route, Routes } from "react-router";
 import "./App.css";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
-import { Textarea } from "./components/ui/textarea";
-import { Toaster } from "./components/ui/sonner";
-import { cn } from "./lib/utils";
-import { toast } from "sonner";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "./components/ui/carousel";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "./components/ui/popover";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-import { ArrowBigRight } from "lucide-react";
+import Indexpage from "./pages/index-page";
+import SignInPage from "./pages/sign-in-page";
+import SignUpPage from "./pages/sign-up-page";
+
+function AuthLayout() {
+  return (
+    <div>
+      <header>Auth!</header>
+      <Outlet />
+    </div>
+  );
+}
 
 function App() {
-  const isActive = true;
   return (
-    <div className="p-5">
-      <ArrowBigRight />
-      <AlertDialog>
-        <AlertDialogTrigger>Open Alert Dialog</AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Title</AlertDialogTitle>
-            <AlertDialogDescription>Description</AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel
-              onClick={() => {
-                console.log("Action");
-              }}
-            >
-              Cancel
-            </AlertDialogCancel>
-            <AlertDialogAction>Continue</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-
-      <Dialog open={false}>
-        <DialogTrigger>Open Dialog</DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Title</DialogTitle>
-            <DialogDescription>Description</DialogDescription>
-          </DialogHeader>
-          <div>Body</div>
-        </DialogContent>
-      </Dialog>
-
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button>Open</Button>
-        </PopoverTrigger>
-        <PopoverContent>Content!</PopoverContent>
-      </Popover>
-
-      <Carousel className="mx-10">
-        <CarouselContent>
-          <CarouselItem className="basis-1/3">1</CarouselItem>
-          <CarouselItem>2</CarouselItem>
-          <CarouselItem>3</CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-
-      <Toaster />
-
-      <Textarea />
-
-      <Input placeholder="입력하세요" />
-
-      <Button
-        onClick={() => {
-          toast("토스트 메시지", {
-            position: "top-center",
-          });
-        }}
-      >
-        버튼
-      </Button>
-      <Button variant={"destructive"}>버튼</Button>
-      <Button variant={"ghost"}>버튼</Button>
-      <Button variant={"link"}>버튼</Button>
-      <Button variant={"outline"}>버튼</Button>
-      <Button variant={"secondary"}>버튼</Button>
-
-      <div
-        className={cn(
-          "w-10 text-2xl font-extrabold",
-          isActive ? "text-green-500" : "text-red-500",
-        )}
-      >
-        isActive
-      </div>
-
-      <div className="text-primary">primary</div>
-      <div className="text-muted">muted</div>
-      <div className="text-destructive">destructive</div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Indexpage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
+      </Route>
+    </Routes>
   );
 }
 
