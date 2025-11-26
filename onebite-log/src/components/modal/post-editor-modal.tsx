cd25f19ca9cf558c919f8963d7fa238edcb836ab
shs-0108 <csohee108@gmail.com>
2025-11-27 00:32:34 +0900

    포스트 - 작성 중 이탈 방지 기능 구현

--- onebite-log/src/components/modal/post-editor-modal.tsx
+++ onebite-log/src/components/modal/post-editor-modal.tsx
@@ -7,6 +7,7 @@ import { useCreatePost } from "@/hooks/mutations/post/use-create-post";
 import { toast } from "sonner";
 import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 import { useSession } from "@/store/session";
+import { useOpenAlertModal } from "@/store/alert-modal";
 
 type Image = {
   file: File;
@@ -15,6 +16,7 @@ type Image = {
 
 export default function PostEditorModal() {
   const session = useSession();
+  const openAlertModal = useOpenAlertModal();
   const { isOpen, close } = usePostEditorModal();
   const { mutate: createPost, isPending: isCreatePostPending } = useCreatePost({
     onSuccess: () => {
@@ -34,6 +36,17 @@ export default function PostEditorModal() {
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   const handleCloseModal = () => {
+    if (content !== "" || images.length !== 0) {
+      //Alert Modal
+      openAlertModal({
+        title: "게시글 작성이 마무리 되지 않았습니다",
+        description: "이 화면에서 나가면 작성중이던 내용이 사라집니다.",
+        onPositive: () => {
+          close();
+        },
+      });
+      return;
+    }
     close();
   };
 
