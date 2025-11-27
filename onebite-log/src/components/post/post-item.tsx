e94ee45839ca2da8563dc5140f5cb869f011cbb6
shs-0108 <csohee108@gmail.com>
2025-11-27 23:12:29 +0900

    포스트 - 수정 기능 구현

--- onebite-log/src/components/post/post-item.tsx
+++ onebite-log/src/components/post/post-item.tsx
@@ -4,6 +4,7 @@ import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 import defaultAvatar from "@/assets/default-avatar.jpg";
 import { Button } from "../ui/button";
 import { formatTimeAgo } from "@/lib/time";
+import EditPostItemButton from "./edit-post-item-button";
 
 export default function PostItem(post: Post) {
   return (
@@ -29,9 +30,7 @@ export default function PostItem(post: Post) {
 
         {/* 1-2. 수정/삭제 버튼 */}
         <div className="text-muted-foreground flex text-sm">
-          <Button className="cursor-pointer" variant={"ghost"}>
-            수정
-          </Button>
+          <EditPostItemButton {...post} />
           <Button className="cursor-pointer" variant={"ghost"}>
             삭제
           </Button>
--- onebite-log/src/store/post-editor-modal.ts
+++ onebite-log/src/store/post-editor-modal.ts
@@ -1,15 +1,36 @@
 import { create } from "zustand";
 import { combine, devtools } from "zustand/middleware";
 
+type CreateMode = {
+  isOpen: true;
+  type: "CREATE";
+};
+type EditMode = {
+  isOpen: true;
+  type: "EDIT";
+  postId: number;
+  content: string;
+  imageUrls: string[] | null;
+};
+
+type OpenState = CreateMode | EditMode;
+type ClostState = { isOpen: false };
+
+type State = ClostState | OpenState;
+
 const initialState = {
   isOpen: false,
-};
+} as State;
+
 const usePostEditorModalStore = create(
   devtools(
     combine(initialState, (set) => ({
       actions: {
-        open: () => {
-          set({ isOpen: true });
+        openCreate: () => {
+          set({ isOpen: true, type: "CREATE" });
+        },
+        openEdit: (param: Omit<EditMode, "isOpen" | "type">) => {
+          set({ isOpen: true, type: "EDIT", ...param });
         },
         close: () => {
           set({ isOpen: false });
@@ -20,15 +41,19 @@ const usePostEditorModalStore = create(
   ),
 );
 
-export const useOpenPostEditorModal = () => {
-  const open = usePostEditorModalStore((store) => store.actions.open);
-  return open;
+export const useOpenCreatePostModal = () => {
+  const openCreate = usePostEditorModalStore(
+    (store) => store.actions.openCreate,
+  );
+  return openCreate;
+};
+
+export const useOpenEditPostModal = () => {
+  const openEdit = usePostEditorModalStore((store) => store.actions.openEdit);
+  return openEdit;
 };
 
 export const usePostEditorModal = () => {
-  const {
-    isOpen,
-    actions: { open, close },
-  } = usePostEditorModalStore();
-  return { isOpen, open, close };
+  const store = usePostEditorModalStore();
+  return store as typeof store & State;
 };
