6f0e268e7c4919fa4e271d96b0ebef78c44275ef
shs-0108 <csohee108@gmail.com>
2025-12-01 17:06:55 +0900

    댓글 조회 기능 구현

--- onebite-log/src/components/comment/comment-list.tsx
+++ onebite-log/src/components/comment/comment-list.tsx
@@ -1,11 +1,23 @@
 import CommentItem from "@/components/comment/comment-item";
+import { useCommentData } from "@/hooks/mutations/comment/use-comment-data";
+import Fallback from "../fallback";
+import Loader from "../loader";
 
-export default function CommentList() {
+export default function CommentList({ postId }: { postId: number }) {
+  const {
+    data: comments,
+    error: fetchCommentsError,
+    isPending: isFetchCommentsPending,
+  } = useCommentData(postId);
+
+  if (fetchCommentsError) return <Fallback />;
+  if (isFetchCommentsPending) return <Loader />;
+
   return (
     <div className="flex flex-col gap-5">
-      <CommentItem />
-      <CommentItem />
-      <CommentItem />
+      {comments.map((comment) => (
+        <CommentItem key={comment.id} {...comment} />
+      ))}
     </div>
   );
 }
